bcd8a118fad234e63cb46f89a5f86314ccd724db
Engin Yılmaz <engnyy@gmail.com>
2026-03-20 15:31:48 +0300

    Add firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Web Client Config — DO NOT commit this file
+export const firebaseConfig = {
+    apiKey: "AIzaSyALyv9Il8xFscbcCJKxRg63ctxBsbnV698",
+    authDomain: "minglebase-fd3ab.firebaseapp.com",
+    projectId: "minglebase-fd3ab",
+    storageBucket: "minglebase-fd3ab.firebasestorage.app",
+    messagingSenderId: "979459834210",
+    appId: "1:979459834210:web:6f8fd209a902edf35b6c3b",
+    measurementId: "G-BCS9RY6TQ6"
+};
